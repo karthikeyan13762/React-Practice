7f3614e0e54b845bb5e56d5ff0637e1d22589a74
unknown <karthikeya3762@gmail.com>
2024-01-13 20:55:58 +0530

    state with object

--- react-tutorial/src/Comonents/Scooter.jsx
+++ react-tutorial/src/Comonents/Scooter.jsx
@@ -2,21 +2,23 @@ import React, { useState } from 'react';
 
 function Scooter(){
 
-    const[color,setColor]=useState('red');
-    const[brand,setBrand]=useState('Honda');
-    const[modal,setModal]=useState('Activa');
-    const[year,setYear]=useState('2020')
+    // const[color,setColor]=useState('red');
+    // const[brand,setBrand]=useState('Honda');
+    // const[modal,setModal]=useState('Activa');
+    // const[year,setYear]=useState('2020')
     
+
+    const [scooter,setScooter]=useState({color:"Red",brand:"Honda",modal:"Activa",year:'2023'})
 
 
     return(
 
        <div>
             <h1>My Scooter</h1>
-            <p>Color :{color}</p>
-            <p>Brand :{brand}</p>
-            <p>Modal :{modal}</p>
-            <p>Year :{year}</p>
+            <p>Color :{scooter.color}</p>
+            <p>Brand :{scooter.brand}</p>
+            <p>Modal :{scooter.modal}</p>
+            <p>Year :{scooter.year}</p>
 
        </div>
     )
